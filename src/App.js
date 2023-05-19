99be096c3a61519f5e61fb37f4526c8d4b52a573
Frieder Sykora <frieder.sykora@stud.uni-duisburg-essen.de>
2023-05-19 12:20:07 +0200

    change app.js

--- src/App.js
+++ src/App.js
@@ -1,102 +1,35 @@
-import React, { Component, useState } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-//import Accelerometer from './components/Accelometer.js'; 
+import logo from './logo.svg';
+import Accelerometer from './src/react-accelerometer';
 
-function App() {
-    const [items, setItems] = useState([]);
-    const [newItem, setNewItem] = useState('');
-
-    const addItem = (newItem) => {
-    if (newItem.trim() !== '') {
-      setItems([...items, newItem]);
-      setNewItem('');
-      }
-    };
-
-  const handleInputChange = (event) => {
-    setNewItem(event.target.value);
-  };
-
-  function accelerometer() {
-    
-    // Request permission to access accelerometer
-    if (typeof DeviceMotionEvent.requestPermission === 'function') {
-        DeviceMotionEvent.requestPermission()
-          .then(permissionState => {
-            if (permissionState === 'granted') {
-              startLoggingAccelerometer();
-            } else {
-              console.log('Permission to access accelerometer denied.');
-              addItem('Permission to access accelerometer denied.');
-            }
-          })
-          .catch(console.error);
-      } else {
-        console.log('DeviceMotionEvent.requestPermission() is not supported.');
-        addItem('DeviceMotionEvent.requestPermission() is not supported.');
-      }
-    }
-    
-      // Start logging accelerometer data
-      function startLoggingAccelerometer() {
-        if (typeof DeviceMotionEvent === 'function' && typeof DeviceMotionEvent.requestPermission === 'function') {
-          // Register the event listener
-          window.addEventListener('devicemotion', handleAccelerometerData);
-          console.log('Logging accelerometer data...');
-          addItem('Logging accelerometer data...');
-    
-        } else {
-          console.log('DeviceMotionEvent is not supported.');
-          addItem('DeviceMotionEvent is not supported.');
-        }
-      }
-      
-      // Handle accelerometer data
-      function handleAccelerometerData(event) {
-        const { accelerationIncludingGravity, rotationRate } = event;
-      
-        // Log accelerometer data
-        console.log('Acceleration (including gravity):', accelerationIncludingGravity);
-        addItem('Acceleration (including gravity):', accelerationIncludingGravity);
-
-        console.log('Rotation rate:', rotationRate);
-        addItem('Rotation rate:', rotationRate);
-      
-        // You can perform further processing or store the data as needed
-      }
-      
-      // Stop logging accelerometer data
-      function stopLoggingAccelerometer() {
-        window.removeEventListener('devicemotion', handleAccelerometerData);
-        console.log('Stopped logging accelerometer data.');
-        addItem('Stopped logging accelerometer data.');
-
-      }
-    
-
-    return(
-      <div className = "App">
-      
-      <h1>Item List</h1>
-      <input
-        type="text"
-        value={newItem}
-        onChange={handleInputChange}
-        placeholder="Enter an item"
-      />
-      <button onClick={accelerometer}>Add Item</button>
-      <ul>
-        {items.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-
-
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Accelerometer timeout={100} render={({ x, y, z, alpha, beta, gamma, north }) => (
+          <header className="App-header">
+          <img
+            src={logo}
+            className="App-logo"
+            alt="logo"
+            style={{
+              transform: `rotate(${north}deg)`
+            }}
+          />
+          <ul>
+            <li>x: {x}</li>
+            <li>y: {y}</li>
+            <li>z: {z}</li>
+            <li>rotation alpha: {alpha}</li>
+            <li>rotation beta: {beta}</li>
+            <li>rotation gamma: {gamma}</li>
+          </ul>
+          <p> North: {north}</p>
+        </header>
+        )}
+        />
       </div>
-      
     );
   }
-
-
-export default App;
+}
